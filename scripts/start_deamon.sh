a442c728b40e3c69ecea2cad019c62e383e1adf0
0000xFFFF <coldmailgg@gmail.com>
2025-10-25 01:50:23 +0200

    added script for starting daemon

---
+++ scripts/start_deamon.sh
@@ -0,0 +1,9 @@
+##!/usr/bin/env bash
+
+if pgrep -f "wpu-darkscore-select" ; then
+    echo "Process is running"
+else
+    echo "Process is not running, starting..."
+    wpu-darkscore-select -i "/media/SSD/media/bg/wpu-darkscore_output.csv" -e plasma-apply-wallpaperimage -l -d
+fi
+
